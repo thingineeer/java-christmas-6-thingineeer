39afda5bdabcd2d888c1b3f4760f231e58e00a6d
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-11 21:18:39 +0900

    feat(Utils) : 입력 값 Map으로 변환하는 기능 구현

---
+++ src/main/java/christmas/Utils.java
@@ -0,0 +1,21 @@
+package christmas;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public final class Utils {
+
+    public static Map<String, Integer> makeStringToHashMap(String input) {
+        Map<String, Integer> resultMap = Stream.of(input.split(","))
+                .map(s -> s.split("-"))
+                .collect(Collectors.toMap(
+                        arr -> arr[0],
+                        arr -> Integer.parseInt(arr[1]),
+                        (existing, replacement) -> existing,
+                        HashMap::new
+                ));
+        return resultMap;
+    }
+}
