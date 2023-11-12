e9678457cba8c6bdea42288f9fbdd312552c1bcf
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-12 15:22:59 +0900

    refactor(ErrorMessage) : Enum 으로 변경

--- src/main/java/christmas/ErrorMessage.java
+++ src/main/java/christmas/ErrorMessage.java
@@ -1,21 +1,21 @@
 package christmas;
 
-public class ErrorMessage {
+public enum ErrorMessage {
 
-    private String PREFIX = "[ERROR] ";
-    public static final String NOT_NUMERIC = "숫자가 아닌 값을 입력 했습니다.";
-
-    public static final String DATE_OUT_OF_RANGE =
-            "[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.";
-    public static final String NOT_VALIDATED_ORDER = "유효하지 않은 주문입니다. 다시 입력해 주세요.";
+    PREFIX_STRING("[ERROR] "),
+    NOT_NUMERIC("숫자가 아닌 값을 입력 했습니다."),
+    DATE_OUT_OF_RANGE("유효하지 않은 날짜입니다. 다시 입력해 주세요."),
+    NOT_VALIDATED_ORDER("유효하지 않은 주문입니다. 다시 입력해 주세요."),
+    ORDER_ONLY_BEVERAGE("음료만 입력한 주문 입니다. 다시 입력해 주세요."),
+    TOTAL_MENU_COUNT_IS_OVER("메뉴 개수의 합이 20개가 초과 합니다. 다시 입력해 주세요.");
     private String message;
 
-    private ErrorMessage(String message) {
+    ErrorMessage(String message) {
         this.message = message;
     }
 
     public String getMessage() {
-        return PREFIX + this.message;
+        return PREFIX_STRING.message + this.message;
     }
 
 
