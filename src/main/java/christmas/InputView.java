29bf10326a4e35ce40fef33fe4d1c8b00362acd8
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-12 15:54:34 +0900

    fix(InputView) : 입력 메세지 상수 처리

--- src/main/java/christmas/InputView.java
+++ src/main/java/christmas/InputView.java
@@ -6,17 +6,19 @@ import christmas.validator.OrderValidator;
 import java.util.Map;
 
 public final class InputView {
+    public static final String READ_DATE_MESSAGE = "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)";
+    public static final String READ_ORDER_MESSAGE = "주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)";
     private static DateValidator dateValidator = new DateValidator();
     public static OrderValidator orderValidator = new OrderValidator();
     public int readDate() {
-        OutputView.printMessage("12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");
+        OutputView.printMessage(READ_DATE_MESSAGE);
         String date = Console.readLine();
         dateValidator.check(date);
         return Integer.parseInt(date);
     }
 
     public Map<String, Integer> readMenuAndQuantityForOrder() {
-        OutputView.printMessage("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+        OutputView.printMessage(READ_ORDER_MESSAGE);
         String orderMenuAndQuantity = Console.readLine();
         orderValidator.check(orderMenuAndQuantity);
         Map<String, Integer> Menu = Utils.makeStringToHashMap(orderMenuAndQuantity);
