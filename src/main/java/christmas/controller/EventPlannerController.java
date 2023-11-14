a1b797a7618f15c270e02fce30b05bc23c7f2754
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-14 16:49:18 +0900

    feat(EventPlannerController) : Controller 구현

--- src/main/java/christmas/controller/EventPlannerController.java
+++ src/main/java/christmas/controller/EventPlannerController.java
@@ -1,8 +1,12 @@
 package christmas.controller;
 
+import christmas.domain.BenefitInformation;
 import christmas.domain.Date;
+import christmas.domain.EventBadge;
+import christmas.domain.GiftMenu;
 import christmas.domain.Order;
 import christmas.domain.OrderManager;
+import christmas.domain.OrderPrice;
 import christmas.domain.OrderRepository;
 import christmas.view.InputView;
 import christmas.view.OutputView;
@@ -12,6 +16,7 @@ public class EventPlannerController {
     private final InputView inputView;
     private final OrderRepository orderRepository;
     private Date date;
+    private int Amount;
 
     public EventPlannerController(InputView inputView, OrderRepository orderRepository) {
         this.inputView = inputView;
@@ -21,21 +26,60 @@ public class EventPlannerController {
     public void run() {
         initialize();
 
-        OrderManager orderinfo = new OrderManager(orderRepository);
-        orderinfo.printAllDetail();
+        OrderManager orderInfo = new OrderManager(orderRepository);
+        orderInfo.printAllDetail();
+
+        OrderPrice orderPriceInfo = new OrderPrice(orderRepository);
+        orderPriceInfo.printBeforeDiscountInfo();
+        Amount = orderPriceInfo.getTotalAmount();
+
+        GiftMenu giftMenu = new GiftMenu(Amount);
+        giftMenu.printGiftInfo();
+
+        BenefitInformation benefitInformation = new BenefitInformation(date, orderRepository);
+        benefitInformation.printDiscountInfo();
+
+        benefitInformation.printTotalDiscount(Amount);
+        benefitInformation.printPaymentAmountAfterDiscount(Amount);
+
+        int result = benefitInformation.calculateTotalDiscount(Amount);
+        EventBadge eventBadge = new EventBadge(result);
+        eventBadge.printEventBadge();
 
     }
 
     private void initialize() {
         OutputView.printWelcome();
-        int dateInput = inputView.readDate();
+        int dateInput = readDateUntilSuccess();
         date = new Date(dateInput);
-        Map<String, Integer> menuQuantityMap = inputView.readMenuAndQuantityForOrder();
+        Map<String, Integer> menuOrder = readMenuAndQuantityForOrderUntilSuccess();
 
-        menuQuantityMap.entrySet().stream()
+        menuOrder.entrySet().stream()
                 .map(menu -> new Order(menu.getKey(), menu.getValue()))
                 .forEach(orderRepository::addOrder);
         OutputView.printBenefit(date.getDate());
+    }
+
+    private int readDateUntilSuccess() {
+        while (true) {
+            try {
+                int dateInput = inputView.readDate();
+                return dateInput;
+            }catch (IllegalArgumentException exception) {
+                OutputView.printMessage(exception.getMessage());
+            }
+        }
+    }
+
+    private Map<String, Integer> readMenuAndQuantityForOrderUntilSuccess() {
+        while (true) {
+            try {
+                Map<String, Integer> menuAndQuantityForOrder = inputView.readMenuAndQuantityForOrder();
+                return menuAndQuantityForOrder;
+            }catch (IllegalArgumentException exception) {
+                OutputView.printMessage(exception.getMessage());
+            }
+        }
     }
 }
 
