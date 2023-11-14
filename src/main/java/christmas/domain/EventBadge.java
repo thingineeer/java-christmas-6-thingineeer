7bd3494d8fa21178e04e55b2180ec830df5ebc2e
LeeMyeongJin <dlaudwls1203@gmail.com>
2023-11-14 16:33:35 +0900

    feat(EventBadge) : 이벤트 배지 기능 구현

--- src/main/java/christmas/domain/EventBadge.java
+++ src/main/java/christmas/domain/EventBadge.java
@@ -1,5 +1,33 @@
 package christmas.domain;
 
+import christmas.view.OutputView;
+
 public class EventBadge {
     // 이벤트 뱃지를 관리 하는 클래스 입니다.
+    private int discountAmount;
+
+    public EventBadge(int discountAmount) {
+        this.discountAmount = discountAmount;
+    }
+
+    public void printEventBadge() {
+        OutputView.printSeparator();
+        OutputView.printMessage(OutputView.EVENT_BADGE_MESSAGE);
+        String badge = determineBadge(discountAmount);
+        OutputView.printMessage(badge);
+    }
+
+    private String determineBadge(int discountAmount) {
+        if (-discountAmount >= 20000) {
+            return "산타";
+        }
+        if (-discountAmount >= 10000) {
+            return "트리";
+        }
+        if (-discountAmount >= 5000) {
+            return "별";
+        }
+        return "없음";
+    }
+
 }
